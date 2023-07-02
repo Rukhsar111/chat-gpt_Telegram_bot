d212ab2e3149549b8db6fa38e8e592aecb3cae99
Rukhsar Ahmed <54540404+Rukhsar111@users.noreply.github.com>
2023-07-02 08:46:44 +0000

    updated

--- src/ChatGpt.py
+++ src/ChatGpt.py
@@ -1,31 +1,38 @@
 import os
 from dotenv import load_dotenv
-from aiogram import bot, dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types
+import openai
 
 
 class Reference:
     """
-    A class to strore the previous response from the chatGPT API
+    A class to store the previous response from the chatGPT API
     """
+    def __init__(self)-> None:
+        self.response=""
 
-    raise NotImplementedError
+
+    
     
 # Load environment variables
 load_dotenv()
 
 # Set up OpenAI API Key
-
+openai.api_key=os.getenv("OPEN_API_KEY")
 
 #Create a reference object to store the previous response
+reference=Reference()
 
 
 # Bot taken cab be obtained via 
-Token=os.getenv("Token") 
+TOKEN=os.getenv("TOKEN") 
 
 # Model uset in chatGPT 
+Model_Name="gpt-3.5-turbo"
+
 
 # initialize bot and dispatcher
-bot=Bot(token=Token)
+bot=Bot(token=TOKEN)
 dispatcher=Dispatcher(bot)
 
 def clear_past():
@@ -33,7 +40,7 @@ def clear_past():
     A function to clear the previous conversations and context.
     """
 
-    raise NotImplementedError
+    reference.response=""
 
 
 @dispatcher.message_handler(commands=['start'])
@@ -42,38 +49,54 @@ async def welcome(message: types.Message):
     A function to welcome the User and clear the previous conversations and context
     """
 
-
-    raise NotImplementedError
+    clear_past()
+    await message.reply(f"Hello! \nI'm  chatGPT Telegram bot created by Rukhsar! \n How may I assist yoy today?")
+    
 
 
 @dispatcher.message_handler(commands=['clear'])
-async def clear(message: types.Message)
+async def clear(message: types.Message):
     """
     A function to clear the previous conversations and context
     """
-
-    raise NotImplementedError
+    clear_past()
+    await message.reply(f"I've cleared the past converstions")
+    
 
 
 @dispatcher.message_handler(commands=['help'])
-async def helper(message: types.Message)
+async def helper(message: types.Message):
     """
     A  handler to display the help menu.
     """
 
-    raise NotImplementedError
+    help_command="""
+    Hi!! I'm a bot created by Rukhsar. Please follow these commands.
+    /start - to start the conversation .
+    /clear - to clear the pat conversations.
+    /help -  to get this help menu.
+    I hope this helps.
+    """
 
+    await message.reply(help_command)
 
-
-
-
+    
 @dispatcher.message_handler()
-async def chatgpt(message: types.Message)
+async def chatgpt(message: types.Message):
     """
-    A handler tp process the users'input and generate a response using the ChatGPT API.
+    A handler to process the users'input and generate a response using the ChatGPT API.
     """
 
-    raise NotImplementedError
+    response=openai.ChatCompletion.create(
+        model=Model_Name,
+        messages=[
+            {"role": "assistant" , "content": reference.response},
+            {"role": "user","content": message.text}
+        ]
+    )
+    reference.response=response['choices'][0]['message']['content']
+    await bot.send_message(chat_id=message.chat.id, text=reference.response)
+
 
 
 
